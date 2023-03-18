45b0cd14236f9dc1fe02c3bac523275db5904371
Ray Brooks <ray.userinterface@gmail.com>
2023-03-18 23:00:16 +0000

    make it nice though

--- packages/client/src/components/TVInputSelector.tsx
+++ packages/client/src/components/TVInputSelector.tsx
@@ -5,39 +5,41 @@ import "@rmwc/switch/styles";
 import { Client } from "../lib";
 
 const availableInputs = [
-  ["DTV", 0, 0],
-  ["Analogue", 1, 0],
-  ["AV", 2, 0],
-  ["Component", 4, 0],
-  ["RGB", 6, 0],
-  ["HDMI 1", 9, 0],
-  ["HDMI 2", 9, 1],
-  ["HDMI 3", 9, 2],
-  ["HDMI 4", 9, 3],
+  ["DTV", "00"],
+  ["Analogue", "10"],
+  ["AV", "20"],
+  ["Component", "40"],
+  ["RGB", "60"],
+  ["HDMI 1", "90"],
+  ["HDMI 2", "91"],
+  ["HDMI 3", "92"],
+  ["HDMI 4", "93"],
 ];
 
-const switchInput = (input: number, port: number = 0) => {
-  return async (event: React.MouseEvent<HTMLAnchorElement>) => {
-    await Client.sendControlRequest(
-      Client.DeviceName.TV,
-      Client.Command.INPUT,
-      `${input}`,
-      `${port}`
-    );
-  };
-};
-
-const getSelectedInput = () => {
-  return Client.getControlRequest(Client.DeviceName.TV, Client.Command.INPUT);
-};
-
 export const TVInputSelector = () => {
-  const [selectedInput, setSelectedInput] = useState(0);
+  const [selectedInput, setSelectedInput] = useState(availableInputs[0][1]);
+
+  const switchInput = (input: string) => {
+    return async (event: React.MouseEvent<HTMLAnchorElement>) => {
+      Client.sendControlRequest(
+        Client.DeviceName.TV,
+        Client.Command.INPUT,
+        input[0],
+        input[1]
+      )
+        .then(({ result }) => {
+          setSelectedInput(result);
+        })
+        .catch((err) => {
+          throw new Error("Cannot set selected input");
+        });
+    };
+  };
 
   useEffect(() => {
-    getSelectedInput()
+    Client.getControlRequest(Client.DeviceName.TV, Client.Command.INPUT)
       .then(({ result }) => {
-        setSelectedInput(parseInt(result, 16));
+        setSelectedInput(result);
       })
       .catch((err) => {
         throw new Error("Cannot get selected input");
@@ -48,14 +50,12 @@ export const TVInputSelector = () => {
     <Box sx={{ ml: 4 }}>
       <ButtonGroup variant="text" aria-label="text button group">
         {availableInputs.map((availableInput) => {
-          const input = availableInput[1] as number;
-          const port = availableInput[2] as number;
           const variant =
-            input * 10 + port === selectedInput ? "contained" : "outlined";
+            availableInput[1] === selectedInput ? "contained" : "outlined";
 
           return (
             <Button
-              onClick={switchInput(input, port)}
+              onClick={switchInput(availableInput[1])}
               href="#"
               variant={variant}
             >
