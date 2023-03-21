24a67723dd6bff4ce347412d532e42eb56345e80
Ray Brooks <ray.userinterface@gmail.com>
2023-03-21 22:23:16 +0000

    handle tv power state at switch parent level. hide other controls when tv power is off

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import { ChangeEvent, useEffect, useState } from "react";
 
 import {
   TopBar,
@@ -8,21 +9,52 @@ import {
   VolumeControl,
   EnergySavingControl,
 } from "./components";
+import { Client } from "./lib";
 
 function App() {
+  const [powerState, setPowerState] = useState(false);
+
+  const powerToggle = async (event: ChangeEvent<HTMLInputElement>) => {
+    Client.sendControlRequest(
+      Client.DeviceName.TV,
+      Client.Command.POWER,
+      !powerState ? "1" : "0"
+    )
+      .then(({ result }) => {
+        setPowerState(parseInt(result, 10) === 1 ? true : false);
+      })
+      .catch((err) => {
+        throw new Error(`Cannot set selected input. ${err}`);
+      });
+  };
+
+  useEffect(() => {
+    Client.getControlRequest(Client.DeviceName.TV, Client.Command.POWER)
+      .then(({ result }) => {
+        setPowerState(parseInt(result, 10) === 1 ? true : false);
+      })
+      .catch((err) => {
+        throw new Error(`Cannot get selected input. ${err}`);
+      });
+  }, []);
+
   return (
     <Container disableGutters sx={{ p: 0 }}>
       <TopBar
         contents={
           <>
-            <TVPowerSwitch />
+            <TVPowerSwitch powerToggle={powerToggle} powerState={powerState} />
           </>
         }
       />
-      <HDMISwitchInputSelector />
-      <TVInputSelector />
-      <VolumeControl />
-      <EnergySavingControl />
+      {powerState && (
+        <>
+          <HDMISwitchInputSelector />
+          <TVInputSelector />
+          <VolumeControl />
+          <EnergySavingControl />
+        </>
+      )}
     </Container>
   );
 }
--- packages/client/src/components/TVPowerSwitch.tsx
+++ packages/client/src/components/TVPowerSwitch.tsx
@@ -1,37 +1,17 @@
-import React, { useEffect } from "react";
+import React, { ChangeEventHandler } from "react";
 import { Typography, Box } from "@mui/material";
 import { Switch } from "@rmwc/switch";
 import "@rmwc/switch/styles";
 
-import { Client } from "../lib";
+export type TVPowerSwitchProps = {
+  powerToggle: ChangeEventHandler<HTMLInputElement>;
+  powerState: boolean;
+};
 
-export const TVPowerSwitch = () => {
-  const [powerState, setPowerState] = React.useState(false);
-
-  const powerToggle = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    Client.sendControlRequest(
-      Client.DeviceName.TV,
-      Client.Command.POWER,
-      !powerState ? "1" : "0"
-    )
-      .then(({ result }) => {
-        setPowerState(result === "01" ? true : false);
-      })
-      .catch((err) => {
-        throw new Error("Cannot set selected input");
-      });
-  };
-
-  useEffect(() => {
-    Client.getControlRequest(Client.DeviceName.TV, Client.Command.POWER)
-      .then(({ result }) => {
-        setPowerState(result === "01" ? true : false);
-      })
-      .catch((err) => {
-        throw new Error("Cannot get selected input");
-      });
-  }, []);
-
+export const TVPowerSwitch = ({
+  powerToggle,
+  powerState,
+}: TVPowerSwitchProps) => {
   return (
     <Box sx={{ ml: 4 }}>
       <Typography variant="button" sx={{ mr: 2 }}>
